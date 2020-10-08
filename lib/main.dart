ff5e4fd83fd22013bf8120a6c16d696475114d7c
cardona326 <sara.cardona@alumnos.uva.es>
2020-10-08 14:02:45 +0200

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
+  var tarea = { "nombre": "Hola soy una tarea", "done": true};
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -10,9 +11,24 @@ class MyApp extends StatelessWidget {
           title: Text('Welcome to Flutter'),
         ),
         body: Center(
-          child: Text('Hello World'),
-        ),
-      ),
-    );
+            child: Container (
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+
+                child: Row(
+                  children: [
+                    if(tarea['done'])
+                      Icon(Icons.done)
+                    else
+                      Icon(Icons.clear),
+                    Text("  "+tarea['nombre']),
+                  ],
+                )
+              )
+            ),
+          ),
+      );
   }
 }
